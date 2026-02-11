f7e359f8318408dc67230e9dd0863e38d66968be
zibin.cai <zibin.cai@speedaf.com>
2026-02-11 16:46:11 +0800

    ai改的流式输出（有问题的版本）

--- src/main/java/com/benson/aicodehelper/service/AiCodeHelperStreamingService.java
+++ src/main/java/com/benson/aicodehelper/service/AiCodeHelperStreamingService.java
@@ -3,11 +3,12 @@ package com.benson.aicodehelper.service;
 import dev.langchain4j.service.MemoryId;
 import dev.langchain4j.service.TokenStream;
 import dev.langchain4j.service.UserMessage;
+import reactor.core.publisher.Flux;
 
 //@AiService(streamingChatModel = "streamingChatModel")
 // @AiService注解会默认将mcp、tool注入，手动创建的不会
 public interface AiCodeHelperStreamingService {
     // 流式对话
-    TokenStream chatStream(@MemoryId int memoryId, @UserMessage String userMessage);
+    Flux<String> chatStream(@MemoryId int memoryId, @UserMessage String userMessage);
 
 }
